28660df87c1a3ae060631fac8ad1c3f68ea7d376
ilia7 <alib327@gmail.com>
2019-06-12 18:04:54 +0430

    initialize models migrations and controllers

---
+++ app/Http/Controllers/ProvinceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\province;
+use Illuminate\Http\Request;
+
+class ProvinceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\province  $province
+     * @return \Illuminate\Http\Response
+     */
+    public function show(province $province)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\province  $province
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(province $province)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\province  $province
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, province $province)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\province  $province
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(province $province)
+    {
+        //
+    }
+}
